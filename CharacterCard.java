34dad25452e254195fcc57475d03bc746caf20b5
GitCode163 <andreyvize2905@gmail.com>
2026-04-25 16:55:42 +0200

    Add files via upload

---
+++ CharacterCard.java
@@ -0,0 +1,19 @@
+public class CharacterCard {
+    public static void main(String[] args) {
+        String name = "Aragon";
+        String characterClass = "warrior";
+        int level = 15;
+        int health = 100;
+        double gold = 250.75;
+        char rank = "S";
+
+        System.out.println("----Character card----");
+        System.out.println("Name: " + name);
+        System.out.println("Character Class: " + characterClass);
+        System.out.println("Level: " + level);
+        System.out.println("Health: " + health);
+        System.out.println("Coin reserve: " + gold);
+        System.out.println("Character rank: " + rank);
+
+    }
+}
